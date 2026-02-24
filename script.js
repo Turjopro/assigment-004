12db36cb11419c22a26e1320f10f894156d2b008
Turjopro <mdtatrimtafsir@gmail.com>
2026-02-24 12:24:27 +0600

    html edit and script all write

--- script.js
+++ script.js
@@ -25,3 +25,131 @@ function calculateCount() {
 }
 
 calculateCount();
+
+// Interview button ---------->>>>>
+
+
+const interviewButtons = document.querySelectorAll('.interview-btn');
+
+for (let i = 0; i < interviewButtons.length; i++) {
+
+    interviewButtons[i].addEventListener('click', function () {
+
+        const jobCard = this.closest('.job');
+        const status = jobCard.querySelector('.status');
+
+        if (jobCard.dataset.status === "Interview") {
+            return;
+        }
+
+        jobCard.dataset.status = "Interview";
+
+        status.innerText = "Interview";
+        status.classList.remove('text-red-500');
+        status.classList.add('text-green-600');
+
+        calculateCount();
+    });
+
+}
+
+// rejected button --------->>>>>>
+
+
+const rejectedButtons = document.querySelectorAll('.rejected-btn');
+
+for (let i = 0; i < rejectedButtons.length; i++) {
+
+    rejectedButtons[i].addEventListener('click', function () {
+
+        const jobCard = this.closest('.job');
+        const status = jobCard.querySelector('.status');
+
+        if (jobCard.dataset.status === "Rejected") {
+            return;
+        }
+
+        jobCard.dataset.status = "Rejected";
+
+        status.innerText = "Rejected";
+        status.classList.remove('text-green-600');
+        status.classList.add('text-red-500');
+
+        calculateCount();
+    });
+
+}
+
+
+// Delete BUtton ------>>>>
+
+
+const deleteButtons = document.querySelectorAll('.btn-dlt');
+
+for (let i = 0; i < deleteButtons.length; i++) {
+
+    deleteButtons[i].addEventListener('click', function () {
+
+        const jobCard = this.closest('.job');
+        jobCard.remove();
+
+        calculateCount();
+    });
+
+}
+ // TAb toggle----->>>
+
+ function toggleStyle(id) {
+
+    allBtn.classList.remove('bg-blue-500', 'text-white');
+    interviewBtn.classList.remove('bg-blue-500', 'text-white');
+    rejectedBtn.classList.remove('bg-blue-500', 'text-white');
+
+    const activeBtn = document.getElementById(id);
+    activeBtn.classList.add('bg-blue-500', 'text-white');
+}
+
+// Filter Tab ----->>>>
+
+allBtn.addEventListener('click', function () {
+
+    toggleStyle('all-btnn');
+
+    const jobs = document.querySelectorAll('.job');
+
+    for (let i = 0; i < jobs.length; i++) {
+        jobs[i].style.display = 'flex';
+    }
+});
+interviewBtn.addEventListener('click', function () {
+
+    toggleStyle('interview-btn');
+
+    const jobs = document.querySelectorAll('.job');
+
+    for (let i = 0; i < jobs.length; i++) {
+
+        if (jobs[i].dataset.status === "Interview") {
+            jobs[i].style.display = 'flex';
+        } else {
+            jobs[i].style.display = 'none';
+        }
+    }
+});
+
+
+rejectedBtn.addEventListener('click', function () {
+
+    toggleStyle('rejected-btn');
+
+    const jobs = document.querySelectorAll('.job');
+
+    for (let i = 0; i < jobs.length; i++) {
+
+        if (jobs[i].dataset.status === "Rejected") {
+            jobs[i].style.display = 'flex';
+        } else {
+            jobs[i].style.display = 'none';
+        }
+    }
+});
